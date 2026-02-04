f5f336bc3a4ba91c3193448470a029792ecf7636
FabTam <fabiotamma90@gmail.com>
2026-02-04 11:27:45 +0100

    Esercitazione-Dati-Complessi

--- Esercitazioni/Esercitazione-03/Program.cs
+++ Esercitazioni/Esercitazione-03/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* ESERCITAZIONE SUI TIPI DI DATI COMPLESSI IN C#
+ Creiamo una applicazione console che dichiara un array di 5 numeri interi, li inizializza con valori a scelta e stampa il primo numero dell'array.
+ */
+using System.Runtime.CompilerServices;
+
+int[] numeri = {1,2,3,4,5};
+ Console.WriteLine(numeri[0]); // Inserire un indice fuori dall'array otteniamo un'eccezione.
+
+ // Dichiaro una lista di interi e stampo il primo elemento della lista.
+
+ List<int> interi = new List<int>{10,20,30};
+ Console.WriteLine(interi[1]);
+
+// Dichiaro una lista di stringhe vuota e aggiungo 1 nome a scelta, stampando in console il secondo nome della lista.
+
+List<string> nomi = new List<string>{"Nome1","Nome2", "Nome3"};
+Console.WriteLine(nomi[1]);
+nomi.Add("Nome4"); // aggiungo un nome alla lista
+Console.WriteLine(nomi[3]);
