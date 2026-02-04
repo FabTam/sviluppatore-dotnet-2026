2f4b2727803a4d1399f9120aa71d71813619b8ee
FabTam <fabiotamma90@gmail.com>
2026-02-04 09:39:16 +0100

    Esercitazione 02

--- Esercitazioni/Esercitazione-02/Program.cs
+++ Esercitazioni/Esercitazione-02/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Addio!");
+Console.Write("Qual è il tuo nome? ");
+string nome = Console.ReadLine();
+Console.Write("Qual è il tuo cognome? ");
+string cognome = Console.ReadLine();
+
+Console.WriteLine($"Benvenuto {nome} {cognome}!");
