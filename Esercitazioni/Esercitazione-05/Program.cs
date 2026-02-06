a02945a13861dcc4cddc3aba6b9b90e5c68ebe40
FabTam <fabiotamma90@gmail.com>
2026-02-06 12:24:43 +0100

    Gestione Eccezioni

--- Esercitazioni/Esercitazione-05/Program.cs
+++ Esercitazioni/Esercitazione-05/Program.cs
@@ -1,63 +1,15 @@
-﻿//LOGICA CONDIZIONALE
+﻿//GESTIONE DELL'ECCEZIONE
+Console.Write("Inserisci il numero da sommare:");
 
-int eta = 20;
+const int NUMERO_PREDEFINITO = 10;
+string input = Console.ReadLine();
 
-if(eta >= 18)
+if(int.TryParse(input, out int numeroUtente))
 {
-    Console.WriteLine("La persona è maggiorenne"); // Verifico l'età, SE maggiore o uguale a 18 stampo l'argomento del Writeline.
-}
-
-int etaPersona = 17;
-if(etaPersona >= 18)
-{
-    Console.WriteLine("La persona è maggiorenne");
+    int risultato = NUMERO_PREDEFINITO + numeroUtente;
+    Console.WriteLine($"Questo è il risultato: {risultato}"); // se la conversione ha successo, sommare il numero predefinito.
 }
 else
 {
-    Console.WriteLine("La persona è minorenne"); // l'else valuta la condizione false dell'if.
-}
-
-int punteggio = 75;
-
-if( punteggio >= 90)
-{
-    Console.WriteLine("Voto: A");
-}
-else if( punteggio >= 80)
-{
-    Console.WriteLine("Voto: B");
-}
-else if(punteggio >= 70)
-{
-    Console.WriteLine("Voto: C");
-}
-else if(punteggio >= 60)
-{
-    Console.WriteLine("Voto: D");    
-}
-else
-{
-    Console.WriteLine("Voto:F");
-}
-
-// FIZZ BUZZ
-
-Console.WriteLine("Digita un numero:");
-int numero = int.Parse(Console.ReadLine());
-
-if ( (numero % 3 == 0) && (numero % 5 == 0) )
-{
-    Console.WriteLine("FizzBuzz");
-}
-else if(numero % 3 == 0)
-{
-    Console.WriteLine("Fizz");
-}
-else if ( numero % 5 == 0)
-{
-    Console.WriteLine("Buzz");
-}
-else
-{
-    Console.WriteLine($"Il numero è: {numero}");
+    Console.WriteLine("ERRORE: l'input inserito non è un numero valido."); // se la conversione fallisce, stampa un messaggio di errore.
 }
