8474a0958d03fc07f9e3cfecab1959958d81bfdb
FabTam <fabiotamma90@gmail.com>
2026-02-09 11:58:05 +0100

    Esercizi cicli terzo commit

--- Esercitazioni/Esercitazione-07/Program.cs
+++ Esercitazioni/Esercitazione-07/Program.cs
@@ -2,18 +2,23 @@
 // ESERCITAZIONE CON CICLO FOREACH
 
 List<int> numeri = new List<int>();
-Console.Write("Inserisci un numero:");
-int inserimento = int.Parse(Console.ReadLine());
-numeri.Add(inserimento);
-while (inserimento != 0)
+
+do
 {
     Console.Write("Inserisci un numero: "); 
-    inserimento = int.Parse(Console.ReadLine());
+    int inserimento = int.Parse(Console.ReadLine());
+    if(inserimento == 0)
+    {
+        break; // esce dal ciclo
+    }
     numeri.Add(inserimento); // aggiungo il numero inserito e ciclo la domanda e l'inserimento fin quando non inserisco 0
 }
+while(true);
+
+Console.WriteLine("I numeri inseriti sono:");
 foreach(var numero in numeri)
 {
-    Console.Write($"{numero},"); // stampo a console la lista 
+    Console.Write($"{numero}"); // stampo a console la lista 
 }
 
 
