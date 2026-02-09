c074f6eed6986aed3753e77016e8c4ccad829cf9
FabTam <fabiotamma90@gmail.com>
2026-02-09 09:57:59 +0100

    Cicli

--- Esercitazioni/Esercitazione-05/Program.cs
+++ Esercitazioni/Esercitazione-05/Program.cs
@@ -1,21 +1,5 @@
-﻿// SWITCH (CONDIZIONALI) ALTERNATIVO A IF ELSE, MA PIù COMODO SUI SINGOLI VALORI
+﻿// FIZZ BUZZ SWITCH
 
-Console.Write("Scegli una opzione fra 1, 2 o 3");
-int scelta = int.Parse(Console.ReadLine());
+Console.Write("Inserisci un numero");
 
-switch(scelta)
-{
-    case 1:
-       Console.WriteLine("Hai scelto l'opzione 1.");
-       break;
-    case 2:
-       Console.WriteLine("Hai scelto l'opzione 1.");
-       break;
-    case 3:
-       Console.WriteLine("Hai scelto l'opzione 1.");
-       break;
-    default:
-       Console.WriteLine("Scelta non valida");
-    break;
-}
 
---
+++ Esercitazioni/Esercitazione-06/Program.cs
@@ -0,0 +1,47 @@
+﻿// CICLI
+
+for(int i = 1; i <= 10; i ++) // Ciclo for{inizializzazione, condizione, incremento}
+{
+    Console.WriteLine(i);
+}
+
+
+int[] numeri = [];
+foreach( var numero in numeri)
+{
+    // blocco di codice da eseguire (Ciclo foreach{ var elemento in collezione})
+}
+
+List<string> nomi = new List<string>{"Nome1", "Nome2","Nome3"};
+
+foreach(var nome in nomi)
+{
+    Console.WriteLine(nome);
+}
+
+Dictionary<int,string> dizionario = new Dictionary<int, string>
+{
+    {1, "Uno"},
+    {2, "Due"},
+    {3, "Tre"}
+
+};
+
+foreach(var kvp in dizionario)
+{
+    Console.WriteLine($"Chiave: {kvp.Key}, Valore: {kvp.Value}"); // kvp è una variabile che rappresenta ogni coppia chiave-valore del dizionario.
+}
+
+int j = 1;
+while(j <= 10)
+{
+    Console.WriteLine(j); // While(condizione) cicla fintanto che la condizione sia vera.
+    j++;
+}
+
+int indice = 1;
+do
+{
+    Console.WriteLine(indice);// Esegue il ciclo almeno una volta, poi verifica la condizione.
+}
+while(indice <= 10);
