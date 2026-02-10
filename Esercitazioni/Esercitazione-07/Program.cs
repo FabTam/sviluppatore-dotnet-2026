dbc10d83a390146a02c4db0f4f784559d0a4a857
FabTam <fabiotamma90@gmail.com>
2026-02-10 10:34:43 +0100

    Commit 10/02/26

--- Esercitazioni/Esercitazione-07/Program.cs
+++ Esercitazioni/Esercitazione-07/Program.cs
@@ -1,64 +1,43 @@
-﻿// RANDOM, genera i numeri in un intervallo semi aperto
-using System.Collections;
+﻿/*
+Chiede all'utente di lanciare un dado per 5 volte(premendo 1 come prima)
+Dopo ogni lancio chiede all'utente se vuole tenere il risultato del lancio(premendo 1 per tenere e 0 per scartare)
+aggiunge ad una lista i risultati dei lanci tenuti
+stampa la lista dei risultati tenuti e la somma dei punteggi 
+*/
 
-Random random = new Random(); // creo una istanza della classe Random, cioè un oggetto "random"
+Random random = new Random();
+List<int> lanciTenuti = new List<int>();
+int somma = 0;
 
- int numeroCasuale = random.Next(1,101); // genera un numero tra 1 e 100, esclude il massimo.
-
- int numeroCasualeSenzaMinimo = random.Next(100); // genera un numero tra 0 e 99, 100 numeri.
-
- double numeroDecimaleCasuale = random.NextDouble(); // genera un numero decimale tra 0,0 e 1,0.
-
- bool booleanoCasuale = random.Next(2) == 0; // Genera un valore booleano casuale(true o false).
-
- // esercitazione dado da gioco
-
- int dado = random.Next(1,7);
- Console.WriteLine($"Hai lanciato il dado e hai ottenuto: {dado}");
-
- // esercitazione dado versione 2
- Console.WriteLine("Lancia il dado con 1:");
- 
-while(true)
+for(int i = 1; i <= 5; i++)
 {
-  string input = Console.ReadLine();
-  int dado2 = random.Next(1,7);
-  if(input == "1")
+  Console.WriteLine("Premi 1 per lanciare il dado: ");
+  string inputLancio = Console.ReadLine();
+  int dado = random.Next(1,7);
+  Console.WriteLine("Vuoi tenere il lancio? premi 1 per tenere, 0 per scartare ");
+  string inputCollettore = Console.ReadLine();
+
+  if(inputCollettore == "1")
   {
-    Console.WriteLine($"Hai lanciato e hai ottenuto: {dado2}");
+    Console.WriteLine($"Hai lanciato il dado e hai ottenuto: {dado}");
+    lanciTenuti.Add(dado);
+    
   }
-  else if( input == "0")
-    {
-        break;
-    }
+  else if(inputCollettore == "0")
+  {
+    Console.WriteLine("Lancio scartato");
+  }
   else
   {
-    Console.WriteLine("Scelta non valida.Riprova");
+    Console.WriteLine("Comando non riconosciuto");
   }
 }
-Console.WriteLine("Gioco terminato");
-
-/* esercitazione DADO v3
- lancio per 5 volte, aggiungo i lanci maggiori o uguali a 5, stampo la lista dei lanci
-*/
-List<int> listaLanci = new List<int>();
-int indice = 1;
-
-for(indice = 1; indice <= 5; indice ++)
-{
-  string input = Console.ReadLine();
-  if(input == "1")
+  
+foreach(var lancio in lanciTenuti)
   {
-    int dado3 = random.Next(1,7);
-    Console.WriteLine($"Hai lanciato e hai ottenuto: {dado3}");
-    if(dado3 >= 5)
-    {
-      listaLanci.Add(dado3); // discrimino i lanci minori di 5 per l'aggiunta alla mia lista
-    }
+   
+   Console.WriteLine($"La lista dei risultati ottenuti è:{lancio}");
+   somma += lancio;
+
   }
- 
-}
-foreach(var lancio in listaLanci)
-{
-  Console.WriteLine($"Ecco la lista dei lanci: {lancio}"); // itero nella lista e stampo a console i lanci
-}
+Console.Write($"La somma dei lanci tenuti è: {somma}");  
