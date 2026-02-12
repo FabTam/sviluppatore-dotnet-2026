696e03c2e3560edd0f78df4e3d5694966cde9b4d
FabTam <fabiotamma90@gmail.com>
2026-02-12 12:54:12 +0100

    12/02/2026 esercizio2-List

--- Esercitazioni/Esercitazione-10/Program.cs
+++ Esercitazioni/Esercitazione-10/Program.cs
@@ -1,165 +1,61 @@
-﻿/* METODI LIST
- A differenza degli array le list hanno molti più metodi disponibii per manipolarle. 
- Questo è dovuto al fatto che le liste hanno un numero variabile di elementi.
- Come ogni struttura devono contenere elementi dello stesso tipo.
-*/
-
-/*
-ADD
-aggiunge un elemento alla lista. Se la lista è già piena, viene automaticamente ridimensionata per accogliere il nuovo elemento.
-*/
-
-using System.Data;
-using System.Xml;
-
-List<int> numeri = new List<int>();
-numeri.Add(1);
-numeri.Add(2);
-Console.WriteLine(string.Join(",", numeri));
-
-/*
-COUNT
-Il metodo count restituisce il numero di elementi presenti nella lista.
-*/
-
-Console.WriteLine(numeri.Count);// output : 2
-
-/*
-ADD RANGE
-Il metoto AddRange permettte di aggiungere più elementi alla lista in un colpo solo. Oppure permette di aggiungere ad una lista un'altra lista
-*/
-numeri.AddRange([3,4,5,6]);
-numeri.AddRange(7,8,9);
-Console.WriteLine(string.Join(",", numeri));
-
-List<int> numeri2 = new List<int>(){10,11,12};
-numeri.AddRange(numeri2);
-Console.WriteLine(string.Join(",", numeri));
-
-/*
-CONTAINS
-Verifica se l'elemento è presente nella lista. Restituisce un true o false(booleano)
-*/
-
-Console.WriteLine(numeri.Contains(3)); // output: true
-
-/*
-INDEX OF
-Trova l'indice dell'elemento in una lista(se non trova il valore restituisce -1)
-*/
-
-Console.WriteLine(numeri.IndexOf(5));// output: 4
-Console.WriteLine(numeri.IndexOf(30));// output:-1
-
-/*
-SORT
-Il metodo sort permette di ordinare gli elementi della lista in ordine crescente.
-*/
-
-numeri.Sort();
-Console.WriteLine(string.Join(",",numeri));
-
-/*
-REVERSE
-Il metodo revrse permette di invertire l'ordine degli elementi
-*/
-
-numeri.Reverse();
-Console.WriteLine(string.Join(",",numeri));
-
-/*
-TOARRAY
-Permette di onvertire una lista in un array
-*/
-
-int[] numeriArray = numeri.ToArray();
-Console.WriteLine(string.Join(",", numeriArray));
-
-/* INSERT
-Il Comando Insert permette di inserire un elemento in una posizione specifica della lista.
-*/
-
-numeri.Insert(0,1); // inserisce il numero 1 all'inizio della lista
-Console.WriteLine(string.Join(",", numeri));
-
-
-/*
-REMOVE
-Il metodo remove permette di rimuovere un elemento dalla lista(restituisce un booleano)
-*/
-
-Console.WriteLine(numeri.Remove(5));
-Console.WriteLine(numeri.Remove(39));
-Console.WriteLine(string.Join(",",numeri)); // lista senza il 5
-
-/*
-REMOVEAT
-Il metodo remove permette di rimuovere un elemento dalla lista ad un dato elemento
-*/
-
-numeri.RemoveAt(0);
-Console.WriteLine(string.Join(",",numeri));
-
-/*
-CLEAR
-Il metodo remove permette di rimuovere tutti gli elementi della lista.
-*/
-
-numeri.Clear();
-Console.WriteLine(string.Join(",",numeri));
-
-
-/*
-TRIMEXCESS
-*/
-Console.WriteLine(numeri.Count);
-numeri.RemoveRange(0,0);
-numeri.TrimExcess();
-Console.WriteLine(numeri.Count);
-
-
-
-
+﻿
 /*
 
-ESERCITAZIONE
+ESERCITAZIONE 2
 
 Programma che: 
-- chiede all'utente di inserire nomi ad una lista finchè non viene inserito 0
-- viene estratto un nome casuale dalla lista e stampato a video
-- viene chiesto all'utente se vuole estrarre un altro nome casuale, se "y" viene estratto un altro nome casuale e stampato altrimenti il programma termina
-- il nome estratto deve essere rimosso una volta estratto e stampato.
+- chiede all'utente di inserire il nome di un prodotto, il prezzo e la quantità intervallati da una virgola(esempio "prodotto1,10.5,2") OK
+- viene ripulita la stringa di input dagli spazi bianchi OK
+- viene verificato che l'input sia valido(deve contenere esattamente 3 parti: nome, prezzo e quantità) OK
+- se non è valido viene stampato un messaggio di errore e viene chiesto di inserire nuovamente il prodotto. OK
+- ogni prodotto viene aggiunto ad una lista di stringhe con il formato "nome:prodotto1 - prezzo: 10.5 - quantità:2"
+- viene chiesto all'utente se vuole inserire un altro prodottox
+- se l'utente risponde "y" viene chiesto di inserire un altro prodotto, altrimenti il programma termina x
+- la lista dei prodotti viene ordinata in ordine alfabetico e viene stampata a video
 */
 
-
-List<string> nomi = new List<string>();
-Random random = new Random();
+List<string> prodotti = new List<string>(); // lista per contenere i prodotti inseriti dall'utente.
 
 while(true)
 {
-  Console.Write("Inserisci di seguito il nome da inserire, o inserisci 0 per terminare: ");
-  string nomeDaInserire = Console.ReadLine();
-  if( nomeDaInserire == "0")
+    Console.WriteLine("Inserisci nome del prodotto, il prezzo e la quantità: ");
+    string input = Console.ReadLine();
+    
+    if(string.IsNullOrWhiteSpace(input))
     {
-        break;  // interrompo il ciclo se scrivo zero 
+        Console.WriteLine("input non valido");
+
     }
-  nomi.Add(nomeDaInserire); // inserisco il nome nella lista
-}
 
-while(nomi.Count > 0)
-{
-    int indiceCasuale = random.Next(nomi.Count); // genero un indice causale tra 0 e il numero di elementi della lista -1
-    string nomeEstratto = nomi[indiceCasuale];
+    string[] prodottoDiviso = input.Split(",");
+    input.Trim();
+    if(prodottoDiviso.Length != 3)
+    {
+        Console.WriteLine("Errore");
+        continue;
 
-    Console.WriteLine($" Nome estratto: {nomeEstratto}");
-    nomi.RemoveAt(indiceCasuale);// rimuove il nome estratto dalla lsita che corrisponde all'indice causale generato
+    }
+     
+    prodotti.Add($"nome: {prodottoDiviso[0]} - prezzo: {prodottoDiviso[1]} - quantità: {prodottoDiviso[2]}");
 
-    Console.WriteLine("Vuoi estrarre un altro nome?(y/n)");
+    Console.WriteLine("Vuoi inserire un altro prodotto? inserisci? (y/n)");
     string risposta = Console.ReadLine();
-
-    if(risposta.ToLower() != "y")
+    if(risposta != "y")
     {
         break;
     }
+    
+   
 }
+prodotti.Sort();
+foreach(string p in prodotti)
+{
+    Console.WriteLine(p);
+}
+
+
+
+
+
+
 
