2de05aed0ffc116518dbaa0a1332d25b89441185
FabTam <fabiotamma90@gmail.com>
2026-02-16 12:30:12 +0100

    Commit 16/02/2026 Esercizi

--- Esercitazioni/Esercitazione-10/Program.cs
+++ Esercitazioni/Esercitazione-10/Program.cs
@@ -1,35 +1,38 @@
 ﻿
 /*
 
-ESERCITAZIONE 2
+ESERCITAZIONE 3
 
 Programma che: 
-- genera una password casuale di lunghezza compresa tra i 5 e gli 8 caratteri, che deve contenere almeno una lettera maiuscola, una lettera minuscola,
- un numero e un carattere speciale(es: @,#,!,ecc). Non deve contenere spazi.
+
+Legge un elenco di email da una lista, per ognuna maschera la parte dell'utente(prima della @) lasciando visibile solo la prima lettera e l'ultima lettera
+mantenendo visibile il dominio (es: "j***i@example.com) e la stampa.
+Il numero degli asterischi deve essere uguale al numero di caratteri mascherati.
 
 */
 
+using System.ComponentModel.DataAnnotations;
 
-
-Random random             = new Random(); // creo la classe random per la generazione casuale della password
-
-int numeroCaratteri       = random.Next(5,9);
-string [] caratteriRandom = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-                             "~","!","@","#","$","%", "^","&","*","_","-","+","=","`","|",";","<",">",".","?","/","0","1","2","3","4",
-                            "5","6","7","8","9","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", 
-                            "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-List<string> password    = new List<string>();
-
-for(int i = 0; i < numeroCaratteri; i ++)
+List<string> email = new List<string>()
 {
-  int carattere             = random.Next(caratteriRandom.Length);
-  string generazioneCasuale = caratteriRandom[carattere].Trim();
-  password.Add(generazioneCasuale);   
-}
+    "fabiotamma90@gmail.com","the_shining_surfer@hotmail.it","godrick_griffyndor@yahoo.it",
 
-// manca il controllo da effettuare sugli intervalli che comprendono minuscole, maiuscole, numeri e caratteri speciali
-Console.WriteLine($"La password è:{string.Join("", password)}");
+};
+
+foreach(var e in email)
+{
+  string maschera                 = "";
+  int indiceChiocciola            = e.IndexOf("@"); // trovo l'indice della chiocciola
+  string sottostringaDaMascherare = e.Substring(1, indiceChiocciola -2); // estraggo la sottostringa da mascherare mettendo 1 e l'indice della chiocciola -1
+  
+  for(int i = 0; i < sottostringaDaMascherare.Length; i++)
+    {
+        maschera += "*";
+    }
+    string emailMascherata = e.Replace(sottostringaDaMascherare,maschera);
+    
+   Console.WriteLine(emailMascherata);
+}
 
 
 
