8cd4fa975495a0b5a3d874eeb7a3433366bf8957
FabTam <fabiotamma90@gmail.com>
2026-02-17 22:10:22 +0100

    Commit-da-casa-17/02/2026

--- Esercitazioni/Esercitazione-12/Program.cs
+++ Esercitazioni/Esercitazione-12/Program.cs
@@ -4,7 +4,7 @@
  con le date.
 */
 
-DateTime birthDate = new DateTime(1990,11,1); // il costruttore di DateTime accetta tre parametri: anno, mese, giorno.
+DateTime birthDate = new DateTime(1990, 11, 1); // il costruttore di DateTime accetta tre parametri: anno, mese, giorno.
 Console.WriteLine($"Sei nato il {birthDate}");
 
 // Conversione di date
@@ -13,7 +13,7 @@ DateTime today = DateTime.Today; // Oggi.
 Console.WriteLine($"Oggi è {today}");
 Console.WriteLine($"Oggi è {today.ToShortDateString()}");
 Console.WriteLine($"Oggi è {today.ToLongDateString()}");
-Console.WriteLine($"Oggi è {today.ToString("dd/MM/yyyy")}"); 
+Console.WriteLine($"Oggi è {today.ToString("dd/MM/yyyy")}");
 
 /*
 . dd indica il giorno a due cifre
@@ -26,13 +26,13 @@ Console.WriteLine($"Oggi è {today.ToString("dddd/MMMM/yyyy")}"); // ritorna una
 // PARSE  
 
 string dateString = "2024-12-31"; // data in formato stringa.
-DateTime date    = DateTime.Parse(dateString);// converte la stringa in un oggetto DateTime.
+DateTime date = DateTime.Parse(dateString);// converte la stringa in un oggetto DateTime.
 Console.WriteLine($"La data convertita è : {date}");
 
 // TryParse restituisce true se la conversione è riuscita, altrimenti restituisce false. Il risultato della conversione è restituito dal parametro out
 
 
-if(DateTime.TryParse(dateString, out DateTime data))
+if (DateTime.TryParse(dateString, out DateTime data))
 {
     Console.WriteLine($"La data convertita è : {data}");
 
@@ -68,7 +68,7 @@ Console.WriteLine($"Ieri era : {ieri:dddd}");
 Timespan indica un intervallo di tempo. Ad esempio, possiamo creare un TimeSpan di 2 giorni e sommarlo ad una data.
 */
 
-TimeSpan timeSpan = new TimeSpan(2,0,0,0);//2 giorni
+TimeSpan timeSpan = new TimeSpan(2, 0, 0, 0);//2 giorni
 
 DateTime traDueGiorni = today.Add(timeSpan);
 Console.WriteLine($"Tra due giorni sarà: {traDueGiorni}");
@@ -82,12 +82,12 @@ Console.WriteLine($"Tra due giorni sarà: {traDueGiorni}");
 DateTime data3 = today.AddDays(1);
 int result = DateTime.Compare(today, domani);
 
-if(result < 0)
+if (result < 0)
 {
     Console.WriteLine("La prima data è precedente alla seconda");
 
 }
-else if( result == 0)
+else if (result == 0)
 {
     Console.WriteLine("Le due date sono uguali");
 
@@ -96,7 +96,7 @@ else
 {
     Console.WriteLine("la prima data è successiva alla seconda");
 }
-                                                                 
+
 /*
 ESERCITAZIONE
  Gestione della scadenza di prodotti deperibili
@@ -112,15 +112,15 @@ ESERCITAZIONE
 
 */
 
-Dictionary<string,string> listaProdotti = new Dictionary<string, string>()
+Dictionary<string, string> listaProdotti = new Dictionary<string, string>()
 {
     {"shampoo", "15/01/2026"},
     {"yogurt","17/02/2026"},
     {"macinato di carne bovina","22/02/2026"}
 };
 
-
-while(true)
+bool valido = false;
+while (!valido)
 {
     Console.WriteLine("Digita 1 se vuoi aggiungere un prodotto");
     Console.WriteLine("Digita 2 se vuoi visualizzare tutti i prodotti");
@@ -130,73 +130,96 @@ while(true)
 
     int input = int.Parse(Console.ReadLine());
 
-    switch(input)
+    switch (input)
     {
-      case 1:
-             Console.WriteLine("Inserisci il nome del prodotto da aggiungere:");
-             string prodotto     = Console.ReadLine();
+        case 1:
+            Console.WriteLine("Inserisci il nome del prodotto da aggiungere:");
+            string prodotto = Console.ReadLine();
 
-             Console.WriteLine("Inserisci la data di scadenza nel formato gg/mm/aa");
-             string dataScadenzastringa = Console.ReadLine();
-             if(DateTime.TryParse(dataScadenzastringa, out DateTime dataScadenza))
-              {
-                listaProdotti[prodotto] = dataScadenza.ToString("dd/MM/yyyy");
-              }
-             else
-             {
-                Console.WriteLine("Conversione non riuscita;");
-             }
-      break;
 
-      case 2:
-            foreach(var kvp in listaProdotti)
+            if (listaProdotti.ContainsKey(prodotto))
             {
-                Console.WriteLine($"Prodotto : {kvp.Key}, Scadenza: {kvp.Value}");
+                Console.WriteLine("Prodotto già inserito");
+                break;
             }
+            Console.WriteLine("Inserisci la data di scadenza nel formato gg/mm/aa");
+            string dataScadenzastringa = Console.ReadLine();
 
+            if (DateTime.TryParse(dataScadenzastringa, out DateTime dataScadenza))
+            {
 
+                if (dataScadenza < DateTime.Today)
+                {
+                    Console.WriteLine("Non puoi inserire un prodotto con data di scadenza già passata.");
+                }
+                else
+                {
+                    listaProdotti.Add(prodotto, dataScadenza.ToString("dd/MM/yyyy"));
+                    Console.WriteLine("Prodotto aggiunto correttamente.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Conversione non riuscita;");
+            }
+            break;
 
-      break;
-      
+        case 2:
+            foreach (var kvp in listaProdotti)
+            {
+                if (DateTime.TryParse(kvp.Value, out DateTime dataScadenzaProdotto))
+                {
+
+                    TimeSpan giorniRimamenti = dataScadenzaProdotto - DateTime.Today;
+                    if (giorniRimamenti.Days >= 0 && giorniRimamenti.Days <= 3)
+                    {
+                        Console.WriteLine($"!PRODOTTO IN SCADENZA! Prodotto: {kvp.Key}, Scade il: {dataScadenzaProdotto:dddd/MMMM/yyyy}");
+                    }
+                    else if (giorniRimamenti.Days <= 0)
+                    {
+                        Console.WriteLine($"!PRODOTTO SCADUTO! Prodotto: {kvp.Key}, Scade il: {dataScadenzaProdotto:dddd/MMMM/yyyy}");
+
+                    }
+                    else if (giorniRimamenti.Days >= 0)
+                    {
+                        Console.WriteLine($" Prodotto: {kvp.Key}, Scade il: {dataScadenzaProdotto:dddd/MMMM/yyyy}");
+
+                    }
+                }
+            }
+            break;
+        case 3:
+
+            foreach (var kvp in listaProdotti)
+                if (DateTime.TryParse(kvp.Value, out DateTime dataScadenzaProdotto))
+                {
+                    TimeSpan giorniRimamenti = dataScadenzaProdotto - DateTime.Today;
+                    if (giorniRimamenti.Days >= 0 && giorniRimamenti.Days <= 3)
+                    {
+                        Console.WriteLine($"Prodotto: {kvp.Key}, Scade il: {dataScadenzaProdotto:dd/MM/yyyy}");
+                    }
+                }
+
+            break;
+        case 4:
+            foreach (var kvp in listaProdotti)
+                if (DateTime.TryParse(kvp.Value, out DateTime dataScadenzaProdotto))
+                {
+                    TimeSpan giorniRimamenti = dataScadenzaProdotto - DateTime.Today;
+                    if (giorniRimamenti.Days <= 0)
+                    {
+                        Console.WriteLine($"Prodotto: {kvp.Key}, Scaduto il: {dataScadenzaProdotto:dd/MM/yyyy}");
+                    }
+                }
+            break;
+        case 5:
+            valido = true;
+            break;
+        default:
+            valido = true;
+
+            break;
+
 
     }
 }
-    /*Console.WriteLine("Inserisci il nome di un prodotto da aggiungere alla lista:");
-    string prodotto = Console.ReadLine();
-    Console.WriteLine("Inserisci la data di un prodotto da aggiungere alla lista: gg/mm/aa");
-    string inputData   = Console.ReadLine();
-    listaProdotti.Add(prodotto, inputData);
-    DateTime dataOdierna = DateTime.Today;
-
-    if(DateTime.TryParse(inputData, out DateTime dataScadenza))
-    {
-    TimeSpan giorniMancanti = dataScadenza - dataOdierna;
-    string stato;
-    if(giorniMancanti.TotalDays < 0)
-        {
-            
-        }
-    
-    
-    }
-    else
-    {
-        Console.WriteLine("Conversione non riuscita, inserire un formato valido.");
-    }
-
-
-
-
-}
-
-
-
-
-foreach(var kvp in listaProdotti)
-{
- Console.WriteLine($"Nome prodotto: {kvp.Key}, data: {kvp.Value}");
-}
-*/
-
-
-
