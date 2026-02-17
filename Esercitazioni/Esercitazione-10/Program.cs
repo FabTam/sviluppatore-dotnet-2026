9658368c5205224732e90b74677e1e32d5ea80ac
FabTam <fabiotamma90@gmail.com>
2026-02-17 12:50:32 +0100

    Commit 17/02/2026 Date

--- Esercitazioni/Esercitazione-10/Program.cs
+++ Esercitazioni/Esercitazione-10/Program.cs
@@ -1,38 +1,54 @@
 ﻿
 /*
 
-ESERCITAZIONE 3
+ESERCITAZIONE 4
 
 Programma che: 
 
-Legge un elenco di email da una lista, per ognuna maschera la parte dell'utente(prima della @) lasciando visibile solo la prima lettera e l'ultima lettera
-mantenendo visibile il dominio (es: "j***i@example.com) e la stampa.
-Il numero degli asterischi deve essere uguale al numero di caratteri mascherati.
+Legge un elenco di frasi da una lista, per ognuna genera uno slug(versione adattata ad essere usata in un URL)
+e le stampa. Lo slug generato deve essere così:
+
+- deve essere tutto in minuscolo.
+- deve sostituire gli spazi con trattini.
+- deve rimuovere la punteggiatura.
+- deve rimuovere eventuali trattini doppi.
 
 */
 
-using System.ComponentModel.DataAnnotations;
+using System.Xml;
 
-List<string> email = new List<string>()
+List<string> frasi = new List<string>()
 {
-    "fabiotamma90@gmail.com","the_shining_surfer@hotmail.it","godrick_griffyndor@yahoo.it",
+    "buongiorno, mi chiamo Fabio", "sono nato a Napoli.", "la mia mail è fabiotamma!--90@gmail.com"
+};
+
+List<char> punteggiatura = new List<char>()
+{
+    '!','?','.',',','.'
 
 };
 
-foreach(var e in email)
+foreach (var frase in frasi)
 {
-  string maschera                 = "";
-  int indiceChiocciola            = e.IndexOf("@"); // trovo l'indice della chiocciola
-  string sottostringaDaMascherare = e.Substring(1, indiceChiocciola -2); // estraggo la sottostringa da mascherare mettendo 1 e l'indice della chiocciola -1
-  
-  for(int i = 0; i < sottostringaDaMascherare.Length; i++)
+    string slug = frase.ToLower();
+
+    foreach (char c in punteggiatura)
     {
-        maschera += "*";
+        slug = slug.Replace(c.ToString(), "");
     }
-    string emailMascherata = e.Replace(sottostringaDaMascherare,maschera);
-    
-   Console.WriteLine(emailMascherata);
+
+    slug = slug.Replace(" ", "-");
+
+    while (slug.Contains("--"))
+    {
+        slug = slug.Replace("--", "-");
+    }
+
+    Console.WriteLine(slug);
 }
+
+
+
 
 
 
---
+++ Esercitazioni/Esercitazione-12/Program.cs
@@ -0,0 +1,202 @@
+﻿/*
+ Gestione delle date
+ Le date dipendono dalla localizzazione del sistema operativo, quindi è importante considerare il fuso orario e le impostazioni locali quando si lavora
+ con le date.
+*/
+
+DateTime birthDate = new DateTime(1990,11,1); // il costruttore di DateTime accetta tre parametri: anno, mese, giorno.
+Console.WriteLine($"Sei nato il {birthDate}");
+
+// Conversione di date
+
+DateTime today = DateTime.Today; // Oggi.
+Console.WriteLine($"Oggi è {today}");
+Console.WriteLine($"Oggi è {today.ToShortDateString()}");
+Console.WriteLine($"Oggi è {today.ToLongDateString()}");
+Console.WriteLine($"Oggi è {today.ToString("dd/MM/yyyy")}"); 
+
+/*
+. dd indica il giorno a due cifre
+. MM il mese a due cifre
+. yyyy l'anno a quattro cifre
+*/
+
+Console.WriteLine($"Oggi è {today.ToString("dddd/MMMM/yyyy")}"); // ritorna una stringa con giorno mese e anno scritti per esteso.
+
+// PARSE  
+
+string dateString = "2024-12-31"; // data in formato stringa.
+DateTime date    = DateTime.Parse(dateString);// converte la stringa in un oggetto DateTime.
+Console.WriteLine($"La data convertita è : {date}");
+
+// TryParse restituisce true se la conversione è riuscita, altrimenti restituisce false. Il risultato della conversione è restituito dal parametro out
+
+
+if(DateTime.TryParse(dateString, out DateTime data))
+{
+    Console.WriteLine($"La data convertita è : {data}");
+
+}
+else
+{
+    Console.WriteLine("La conversione della data non è riuscita");
+}
+
+
+// TIME STAMP
+
+/*
+ Molto spesso è utile generare un timestamp, ovvero un numero che rappresenta il numero di secondi trascorsi dal 1 gennaio 1970(Epoch Time)
+*/
+
+DateTimeOffset now = DateTimeOffset.UtcNow;
+long timestamp = now.ToUnixTimeSeconds();
+Console.WriteLine($"Il timestamp attuale è: {timestamp}");
+
+
+// OPERAZIONI CON LE DATE
+
+// SOMMA
+
+DateTime domani = today.AddDays(1);
+Console.WriteLine($"Domani è: {domani}");
+
+DateTime ieri = today.AddDays(-1);
+Console.WriteLine($"Ieri era : {ieri:dddd}");
+
+/*
+Timespan indica un intervallo di tempo. Ad esempio, possiamo creare un TimeSpan di 2 giorni e sommarlo ad una data.
+*/
+
+TimeSpan timeSpan = new TimeSpan(2,0,0,0);//2 giorni
+
+DateTime traDueGiorni = today.Add(timeSpan);
+Console.WriteLine($"Tra due giorni sarà: {traDueGiorni}");
+
+/*
+ Compare
+ Fra il confronto fra due date, restituendo un valore negativo se la prima data è precedente alla seconda, zero se sono uguali e un valore positivo
+ se la prima data è successiva alla seconda.
+*/
+
+DateTime data3 = today.AddDays(1);
+int result = DateTime.Compare(today, domani);
+
+if(result < 0)
+{
+    Console.WriteLine("La prima data è precedente alla seconda");
+
+}
+else if( result == 0)
+{
+    Console.WriteLine("Le due date sono uguali");
+
+}
+else
+{
+    Console.WriteLine("la prima data è successiva alla seconda");
+}
+                                                                 
+/*
+ESERCITAZIONE
+ Gestione della scadenza di prodotti deperibili
+ Programma che:
+ - parte con 3 prodotti di default.
+ - chiede all'utente di inserire il nome di un prodotto(ad un dizionario) e la sua data di scadenza (due input?)
+ - Calcola quanti giorni mancano alla scadenza del prodotto
+ - Stampa una indicazione a fianco al prodotto che indica se il prodotto è scaduto, sta per scadere(entro 3 giorni) o è ancora vendibile.
+ - Stampa la data di scadenza in un formato leggibile( 31 dicembre 2024) scritta fra parentesi.
+ - Il programma impedisce l'inserimento di prodotti con date di scadenza già passate.
+ - il programma consente di visualizzare solo i prodotti che stanno per scadere entro 3 giorni
+ - implementazione di un menù che consente di visualizzare tutti i prodotti o solo quelli in scadenza entro 3 giorni o quelli scaduti
+
+*/
+
+Dictionary<string,string> listaProdotti = new Dictionary<string, string>()
+{
+    {"shampoo", "15/01/2026"},
+    {"yogurt","17/02/2026"},
+    {"macinato di carne bovina","22/02/2026"}
+};
+
+
+while(true)
+{
+    Console.WriteLine("Digita 1 se vuoi aggiungere un prodotto");
+    Console.WriteLine("Digita 2 se vuoi visualizzare tutti i prodotti");
+    Console.WriteLine("Digita 3 se vuoi visualizzare in prodotti in scadenza entro 3 giorni");
+    Console.WriteLine("Digita 4 se vuoi visualizzare i prodotti scaduti");
+    Console.WriteLine("Digita 5 se vuoi uscire");
+
+    int input = int.Parse(Console.ReadLine());
+
+    switch(input)
+    {
+      case 1:
+             Console.WriteLine("Inserisci il nome del prodotto da aggiungere:");
+             string prodotto     = Console.ReadLine();
+
+             Console.WriteLine("Inserisci la data di scadenza nel formato gg/mm/aa");
+             string dataScadenzastringa = Console.ReadLine();
+             if(DateTime.TryParse(dataScadenzastringa, out DateTime dataScadenza))
+              {
+                listaProdotti[prodotto] = dataScadenza.ToString("dd/MM/yyyy");
+              }
+             else
+             {
+                Console.WriteLine("Conversione non riuscita;");
+             }
+      break;
+
+      case 2:
+            foreach(var kvp in listaProdotti)
+            {
+                Console.WriteLine($"Prodotto : {kvp.Key}, Scadenza: {kvp.Value}");
+            }
+
+
+
+      break;
+      
+
+    }
+}
+    /*Console.WriteLine("Inserisci il nome di un prodotto da aggiungere alla lista:");
+    string prodotto = Console.ReadLine();
+    Console.WriteLine("Inserisci la data di un prodotto da aggiungere alla lista: gg/mm/aa");
+    string inputData   = Console.ReadLine();
+    listaProdotti.Add(prodotto, inputData);
+    DateTime dataOdierna = DateTime.Today;
+
+    if(DateTime.TryParse(inputData, out DateTime dataScadenza))
+    {
+    TimeSpan giorniMancanti = dataScadenza - dataOdierna;
+    string stato;
+    if(giorniMancanti.TotalDays < 0)
+        {
+            
+        }
+    
+    
+    }
+    else
+    {
+        Console.WriteLine("Conversione non riuscita, inserire un formato valido.");
+    }
+
+
+
+
+}
+
+
+
+
+foreach(var kvp in listaProdotti)
+{
+ Console.WriteLine($"Nome prodotto: {kvp.Key}, data: {kvp.Value}");
+}
+*/
+
+
+
