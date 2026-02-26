0acfd5af8d698d65c6cdb3efa8ae89c99f389e64
FabTam <fabiotamma90@gmail.com>
2026-02-26 09:02:16 +0100

    Commit 26/02/2026

--- Esercitazioni/Esercitazione-15/Program.cs
+++ Esercitazioni/Esercitazione-15/Program.cs
@@ -1,16 +1,40 @@
 ﻿
 using Newtonsoft.Json;
 
-// SERIALIZZAZIONE DI UN OGGETTO
+/* ESERCITAZIONE 1
+ - Programma che usa un file json come metodo di persistenza dell'id dell'ultimo partecipante creato, in modo da poterlo incrementare ad ogni creazione di un nuovo partecipante.
+ 
+ - Crea un file json chiamato lastId.json con 
+ {
+    "lastId" : 0
+ }
 
-var partecipante = new
-{
-    nome = "Partecipante1",
-    eta = 30,
-    presente = true,
-    interessi = new List<string> {"programmazione", "musica", "sport"}
+ Dizionario int string dove la chiave è lastId e il valore è un int.
+ 
 
-};
+ - L'applicazione legge il file json
+ - deserializza il contenuto in un oggetto
+ - incrementa il valore di lastId
+ - serializza l'oggetto aggiornato
+ - lo scriverà nuovamente sul file
+*/
 
-string json = JsonConvert.SerializeObject(partecipante, Formatting.Indented);
-File.WriteAllText(@"test.json",json);
+
+
+    string path      = @"lastId.json";
+    string json      = File.ReadAllText(path);
+
+    var oggettoJson = JsonConvert.DeserializeObject<dynamic>(json);
+
+    Console.WriteLine("Inserisci il nome di un partecipante:");
+    string input = Console.ReadLine();
+
+    oggettoJson.partecipante = input;
+    oggettoJson.lastId += 1;
+
+    json = JsonConvert.SerializeObject(oggettoJson, Formatting.Indented);
+    File.WriteAllText(path, json);
+
+
+
+
